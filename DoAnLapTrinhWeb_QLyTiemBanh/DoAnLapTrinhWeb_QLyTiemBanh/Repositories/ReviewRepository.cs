dd845dd469ed3ba2b5430b67d4072f83a1765e9f
thuowg203 <buithithuhuong06@gmail.com>
2025-12-25 08:33:07 +0700

    Add model,.. for function

---
+++ DoAnLapTrinhWeb_QLyTiemBanh/DoAnLapTrinhWeb_QLyTiemBanh/Repositories/ReviewRepository.cs
@@ -0,0 +1,25 @@
+﻿using DoAnLapTrinhWeb_QLyTiemBanh.Models;
+using Microsoft.EntityFrameworkCore;
+namespace DoAnLapTrinhWeb_QLyTiemBanh.Repositories
+{
+    public class ReviewRepository : IReviewRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public ReviewRepository(ApplicationDbContext context) => _context = context;
+
+        public async Task AddAsync(ProductReview review)
+        {
+            await _context.ProductReviews.AddAsync(review);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<ProductReview>> GetByProductIdAsync(int productId)
+        {
+            return await _context.ProductReviews
+                .Include(r => r.User)
+                .Where(r => r.ProductId == productId)
+                .OrderByDescending(r => r.CreatedDate)
+                .ToListAsync();
+        }
+    }
+}
